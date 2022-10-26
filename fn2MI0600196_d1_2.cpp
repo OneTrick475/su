11da75cfc6de0bd7c54bb8f8ac78244081e30d28
OneTrick475 <64459686+OneTrick475@users.noreply.github.com>
2022-10-26 21:16:37 +0300

    idk

--- fn2MI0600196_d1_2.cpp
+++ fn2MI0600196_d1_2.cpp
@@ -11,90 +11,88 @@
 *
 */
 
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main() {
-//
-//	enum romans {
-//		I = 1,
-//		IV = 4,
-//		V = 5,
-//		IX = 9,
-//		X = 10,
-//		XL = 40,
-//		L = 50,
-//		XC = 90,
-//		C = 100,
-//		CD = 400,
-//		D = 500,
-//		CM = 900,
-//		M = 1000
-//	};
-//	
-//	int num = 0;
-//
-//	cin >> num;
-//
-//	if (num < 1 || num > 2000) {
-//		cout << "Incorrect input";
-//		return 1;
-//	}
-//
-//
-//	while (num > 0) {
-//		if (num >= M) {
-//			cout << "M";
-//			num -= M;
-//		}
-//		else if (num >= CM) {
-//			cout << "CM";
-//			num -= CM;
-//		}
-//		else if (num >= D) {
-//			cout << "D";
-//			num -= D;
-//		}
-//		else if (num >= CD) {
-//			cout << "CD";
-//			num -= CD;
-//		}
-//		else if (num >= C) {
-//			cout << "C";
-//			num -= C;
-//		}
-//		else if (num >= XC) {
-//			cout << "XC";
-//			num -= XC;
-//		}
-//		else if (num >= L) {
-//			cout << "L";
-//			num -= L;
-//		}
-//		else if (num >= XL) {
-//			cout << "XL";
-//			num -= XL;
-//		}
-//		else if (num >= X) {
-//			cout << "X";
-//			num -= X;
-//		}
-//		else if (num >= IX) {
-//			cout << "IX";
-//			num -= IX;
-//		}
-//		else if (num >= V) {
-//			cout << "V";
-//			num -= V;
-//		}
-//		else if (num >= IV) {
-//			cout << "IV";
-//			num -= IV;
-//		}
-//		else if (num >= I) {
-//			cout << "I";
-//			num -= I;
-//		}
-//	}
-//}
+#include <iostream>
+
+int main() {
+
+	enum romans {
+		I = 1,
+		IV = 4,
+		V = 5,
+		IX = 9,
+		X = 10,
+		XL = 40,
+		L = 50,
+		XC = 90,
+		C = 100,
+		CD = 400,
+		D = 500,
+		CM = 900,
+		M = 1000
+	};
+	
+	int num = 0;
+
+	std::cin >> num;
+
+	if (num < 1 || num > 2000) {
+		std::cout << "Incorrect input";
+		return 1;
+	}
+
+
+	while (num > 0) {
+		if (num >= M) {
+			std::cout << "M";
+			num -= M;
+		}
+		else if (num >= CM) {
+			std::cout << "CM";
+			num -= CM;
+		}
+		else if (num >= D) {
+			std::cout << "D";
+			num -= D;
+		}
+		else if (num >= CD) {
+			std::cout << "CD";
+			num -= CD;
+		}
+		else if (num >= C) {
+			std::cout << "C";
+			num -= C;
+		}
+		else if (num >= XC) {
+			std::cout << "XC";
+			num -= XC;
+		}
+		else if (num >= L) {
+			std::cout << "L";
+			num -= L;
+		}
+		else if (num >= XL) {
+			std::cout << "XL";
+			num -= XL;
+		}
+		else if (num >= X) {
+			std::cout << "X";
+			num -= X;
+		}
+		else if (num >= IX) {
+			std::cout << "IX";
+			num -= IX;
+		}
+		else if (num >= V) {
+			std::cout << "V";
+			num -= V;
+		}
+		else if (num >= IV) {
+			std::cout << "IV";
+			num -= IV;
+		}
+		else if (num >= I) {
+			std::cout << "I";
+			num -= I;
+		}
+	}
+}
