4d7bdd6e2621935f2cd0fc7119b734adcfd6833c
OneTrick475 <64459686+OneTrick475@users.noreply.github.com>
2022-10-26 15:37:53 +0300

    Idk

--- fn2MI0600196_d1_1.cpp
+++ fn2MI0600196_d1_1.cpp
@@ -1,56 +1,40 @@
-/**
-*
-* Solution to homework assignment 1
-* Introduction to programming course
-* Faculty of Mathematics and Informatics of Sofia University
-* Winter semester 2022/2023
-*
-* @author Ivan Bukev
-* @idnumber 2MI0600196
-* @task 1
-*
-*/
+#include <iostream>
 
+using namespace std;
 
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main() {
-//	int n = 0;
-//
-//	cin >> n;
-//
-//	if (n < 1 || n > 1000000) {
-//		cout << "Incorrect input";
-//		return 1;
-//	}
-//
-//	int multiplier = 1;
-// 
-//	const int DIVISOR = 10;
-//
-//	int m = 0;
-//
-//	while (n / DIVISOR != 0 || n % DIVISOR != 0) {
-//		int num = n % DIVISOR;
-//
-//		m += num * num * multiplier;
-//
-//		if (num * num >= 10)
-//			multiplier *= 100;
-//		else
-//			multiplier *= 10;
-//
-//		n /= DIVISOR;
-//	}
-//
-//	m += multiplier / 10;
-//
-//	while (m / 10 != 0 || m % 10 != 0) {
-//		cout << m % 10;
-//
-//		m /= 10;
-//	}
-//
-//}
+int main() {
+	int n = 0;
+
+	cin >> n;
+	
+	if (n < 1 || n > 1000000) {
+		cout << "Incorrect input";
+		return 1;
+	}
+ 
+	const int DIVISOR = 10;
+ 
+    bool printZero = false;
+ 
+	while (n / DIVISOR != 0 || n % DIVISOR != 0) {
+		int num = (n % DIVISOR) * (n % DIVISOR);
+		
+		if(num != 0) {
+		    printZero = true;
+		}
+        else if(printZero){
+            cout<< 0;
+        }
+
+        while(num / DIVISOR != 0 || num % DIVISOR != 0){
+            int currentNum = num % DIVISOR;
+            
+            if(n / DIVISOR == 0 && num / DIVISOR == 0) currentNum++;
+            
+            cout<< currentNum;
+            
+            num /= DIVISOR;
+        }
+        n /= DIVISOR;
+	}
+}
