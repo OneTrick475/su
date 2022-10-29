03fcccea422e8944e46c6867f9bab40640e97cc6
OneTrick475 <64459686+OneTrick475@users.noreply.github.com>
2022-10-29 20:18:22 +0300

    something

--- fn2MI0600196_d1_3.cpp
+++ fn2MI0600196_d1_3.cpp
@@ -11,50 +11,54 @@
 *
 */
 
-//#include <iostream>
-//
-//int main() {
-//	int num = 0;
-//	
-//	std::cin >> num;
-//	
-//	if (num < 2 || num > 9) {
-//		std::cout << "Incorrect input";
-//		return 1;
-//	}
-//	int columns = num * 3;
-//
-//	int colOffset = (80 - columns) / 2;
-//
-//	int rows = num * 2 - 1;
-//
-//	int rowOffset = (25 - rows * 2 - 1) / 2;
-//
-//	for (int i = 0; i < rowOffset; ++i) {
-//		std::cout << "\n";
-//	}
-//
-//	for (int i = 1; i <= rows; ++i) {
-//		for (int i = 0; i < colOffset; ++i) {
-//			std::cout << ' ';
-//		}
-//
-//		int currentNumber = (i <= num) ? i : rows - i + 1;
-//
-//		for (int i = 1; i <= columns; ++i) {
-//			if (i <= currentNumber) {
-//				std::cout << i;
-//			}
-//			else if (i > columns - currentNumber) {
-//				std::cout << columns - i + 1;
-//			}
-//			else {
-//				char fillerCharacter = (currentNumber == num ? '-' : ' ');
-//
-//				std::cout << fillerCharacter;
-//			}
-//		}
-//
-//		std::cout << "\n\n";
-//	}
-//}
+#include <iostream>
+
+int main() {
+	int num = 0;
+	
+	std::cin >> num;
+	
+	if (num < 2 || num > 9) {
+		std::cout << "Incorrect input";
+		return 1;
+	}
+
+	int columns = num * 3;
+
+	int colOffset = (80 - columns) / 2;
+
+	int rows = num * 2 - 1;
+
+	int rowOffset = (25 - rows)/2;
+
+	for (int i = 0; i < rowOffset; ++i) {
+		std::cout << "\n";
+	}
+
+	for (int i = 1; i <= rows; ++i) {
+		for (int i = 0; i < colOffset; ++i) {
+			std::cout << ' ';
+		}
+
+		int currentNumber = (i <= num) ? i : rows - i + 1;
+
+		for (int i = 1; i <= columns; ++i) {
+			if (i <= currentNumber) {
+				std::cout << i;
+			}
+			else if (i > columns - currentNumber) {
+				std::cout << columns - i + 1;
+			}
+			else {
+				char fillerCharacter = (currentNumber == num ? '-' : ' ');
+
+				std::cout << fillerCharacter;
+			}
+		}
+
+		std::cout << "\n";
+	}
+	for (int i = 0; i < rowOffset; ++i) {
+		std::cout << "\n";
+	}
+}
