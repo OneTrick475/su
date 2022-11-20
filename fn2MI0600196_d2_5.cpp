b2376e93ae2cf2b73234e14c28b89c9bc751bd41
OneTrick475 <64459686+OneTrick475@users.noreply.github.com>
2022-11-20 13:59:29 +0200

    Add files via upload

---
+++ fn2MI0600196_d2_5.cpp
@@ -0,0 +1,78 @@
+/**
+*
+* Solution to homework assignment 2
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2022/2023
+*
+* @author Ivan Bukev
+* @idnumber 2MI0600196
+* @task 5
+*
+*/
+
+#include <iostream>
+
+bool isPrime(int num) {
+	if (num == 2) {
+		return true;
+	}
+
+	if (num == 0 || num == 1 || num % 2 == 0) {
+		return false;
+	}
+
+	for (int i = 3; i < sqrt(num) + 1; i += 2) {
+		if (num % i == 0) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+int concat(int numArr[],int start,int end) {
+	int num = 0;
+	int multiplier = 1;
+
+	while (start <= end) {
+		num += (numArr[start++]) * multiplier;
+		multiplier *= 10;
+	}
+
+	return num;
+}
+
+int numOfOperationsToPrime(int num) {
+	const int LOWER_LIMIT = 1;
+	const int UPPER_LIMIT = 1000000000;
+
+	if (num < LOWER_LIMIT || num > UPPER_LIMIT) {
+		return -1;
+	}
+
+	int numArr[10] = {};
+	int size = 0;
+
+	while (num / 10 != 0 || num % 10 != 0) {
+		numArr[size++] = num % 10;
+		num /= 10;
+	}
+
+	for (int i = size - 1; i >= 0; --i) {
+		for (int j = 0; j <= i; j += 2) {
+			int currNum = concat(numArr, j, i);
+			if (isPrime(currNum)) {
+				return (size - 1 - i) + (j / 2);
+			}
+		}
+	}
+	return -1;
+}
+
+int main() {
+	int num = 0;
+
+	std::cin >> num;
+	std::cout << numOfOperationsToPrime(num);
+}
