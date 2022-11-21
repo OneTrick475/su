94837112fa5d8789882d6c72c9a7a16dbc3f1d91
OneTrick475 <64459686+OneTrick475@users.noreply.github.com>
2022-11-21 23:31:28 +0200

    the most disgusting code ive written.. but works i think

--- fn2MI0600196_d1_1.cpp
+++ fn2MI0600196_d1_1.cpp
@@ -11,51 +11,57 @@
 *
 */
 
-#include <iostream>
-
-int main() {
-	int n = 0;
-
-	std::cin >> n;
-
-	if (n < 1 || n > 1000000) {
-		std::cout << "Incorrect input";
-		return 1;
-	}
-
-	const int DIVISOR = 10;
-
-	long long multiplier = 1;
-
-	long long int m = 0;
-
-	while (n / DIVISOR != 0 || n % DIVISOR != 0) {
-		long long num = n % DIVISOR;
-
-		m += num * num * multiplier;
-
-		if (num * num >= 10) {
-			multiplier *= 100;
-		}
-		else {
-			multiplier *= 10;
-		}
-
-		n /= DIVISOR;
-	}
-
-	multiplier /= DIVISOR;
-
-	long long result = 0;
-
-	while (multiplier > 0) {
-		result += m % DIVISOR * multiplier;
-		
-		m /= DIVISOR;
-
-		multiplier /= DIVISOR;
-	}
-
-	std::cout << result + 1;
-
-}
+//#include <iostream>
+//
+//int main() {
+//	int n = 0;
+//
+//	std::cin >> n;
+//
+//	if (n < 1 || n > 1000000) {
+//		std::cout << "Incorrect input";
+//		return 1;
+//	}
+//
+//	long long multiplier = 1;
+// 
+//	const int DIVISOR = 10;
+//
+//	long long int m = 0;
+//
+//	while (n / DIVISOR != 0 || n % DIVISOR != 0) {
+//		long long num = n % DIVISOR;
+//
+//		m += num * num * multiplier;
+//
+//		if (num * num >= 10) {
+//			multiplier *= 100;
+//		}
+//		else {
+//			multiplier *= 10;
+//		}
+//
+//		n /= DIVISOR;
+//	}
+//
+//	m += multiplier / 10;
+//
+//	bool printZero = false;
+//
+//	while (m / 10 != 0 || m % 10 != 0) {
+//		int num = m % 10;
+//
+//		if (num != 0) {
+//			printZero = true;
+//		}
+//		else if (!printZero) {
+//			m /= 10;
+//			continue;
+//		}
+//
+//		std::cout << num;
+//
+//		m /= 10;
+//	}
+//
+//}
