9aece8c3fb2bd92aeb5ff54b823cb407c5964372
OneTrick475 <64459686+OneTrick475@users.noreply.github.com>
2022-12-06 22:07:09 +0200

    Add files via upload

---
+++ fn2MI0600196_d3_1.cpp
@@ -0,0 +1,36 @@
+/**
+*
+* Solution to homework assignment 3
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2022/2023
+*
+* @author Ivan Bukev
+* @idnumber 2MI0600196
+* @task 1
+*
+*/
+
+#include <iostream>
+
+void cypher(char key, char str[]){
+	int i = 0;
+
+	while(str[i] != 0) {
+		std::cout << char(str[i++] ^ key);
+	}
+}
+
+int main(){
+	const int MAX_SIZE = 1025;
+
+	char str[MAX_SIZE];
+
+	char key;
+
+	std::cin >> key;
+
+	std::cin >> str;
+
+	cypher(key, str);
+}
